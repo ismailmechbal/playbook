6a8185b7a3e7eb6befde0514a49370c35aba5d9e
Abraham Kuri <kurenn@icalialabs.com>
2016-09-27 12:34:04 -0500

    Adds new style base typography

--- source/javascripts/playbook.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  $(window).scroll(function(){
-    if($(window).scrollTop() >= 110){
-      $(".logo").addClass('small');
-    } else {
-      $(".logo").removeClass('small');
-    }
-  });
-});
